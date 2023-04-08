6b5a1873b1b86ab15d480e14352303b99b971707
PicadoNavarro <lucia.picado23@gmail.com>
2023-04-08 16:04:13 -0600

    Cards de servicios

--- src/app/panel/servicios/servicios.component.ts
+++ src/app/panel/servicios/servicios.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServiciosComponent implements OnInit {
 
+  longText = `Paramount+ $4.99 al mes. Prime Video: $5.99 a partir del 4to mes. HBO MAX: $5.99 a partir del 2do mes. Todos los precios incluyen IVA`;
+  longText2 = `Paramount+ $4.99 al mes. Prime Video: $5.99 a partir del 4to mes. HBO MAX: $5.99 a partir del 3er mes. ViX+
+  $5.99 a partir del 2do mes. Universal+ $5.90 mes. Todos los precios incluyen IVA`;
   constructor(){}
 
   ngOnInit(): void {
@@ -17,3 +20,6 @@ export class ServiciosComponent implements OnInit {
 
   }
 }
+
+
+
